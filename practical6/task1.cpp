a059e22bd4288757e8182b60f70516b1f460a006
Rainu2003 <86703528+Rainu2003@users.noreply.github.com>
2021-10-06 10:05:39 +0530

    Create task1.cpp

---
+++ practical6/task1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a=10;
+ string c="cplusplus";
+  float b=43.5;
+  cout<<a+45<<endl;
+  cout<<b+4.5<<endl;
+  cout<<a+b<<endl;
+  cout<<a+c<<endl; 
+}
